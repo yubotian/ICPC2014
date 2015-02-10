36c3545b30f6f42cef74ff9eaa334ddb2a5e3308
Yubo Tian <yubo.d.tian@gmail.com>
2015-02-10 11:27:26 -0500

    more

---
+++ src/nwerc2014_G.java
@@ -0,0 +1,14 @@
+/**
+ * Created by Yubo on 2/9/15.
+ *
+ * subproblem:
+ * a) minimum total distance
+ *    - find geometric median (each dimension)
+ *
+ * b) maximum D to everyone for any person
+ *
+ *
+ */
+public class nwerc2014_G {
+
+}
